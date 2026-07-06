dc4cc340964e0b756b7a4b9decc805368d7e6133
Zhang-Xiaoxue <xiaoxuezhang17@outlook.com>
2026-07-06 07:08:09 +0000

    merge and cherry pick deepep graph

--- bashs/setup_sglang_LLaDA2_concurrency_zxx_EP_bf16.sh
+++ bashs/setup_sglang_LLaDA2_concurrency_zxx_EP_bf16.sh
@@ -1,20 +1,20 @@
 #!/bin/bash
 
-export ASCEND_RT_VISIBLE_DEVICES=0,1,2,3
+export ASCEND_RT_VISIBLE_DEVICES=1,2,3,5
 
-#export ASCEND_LAUNCH_BLOCKING=1
-export SGLANG_ALLOW_OVERWRITE_LONGER_CONTEXT_LEN=1
-export SGLANG_NPU_DISABLE_ACL_FORMAT_WEIGHT=1
-unset HTTP_PROXY HTTPS_PROXY http_proxy https_proxy
-export AUTOFUSE_FLAGS="--enable_autofuse=true;--autofuse_enable_pass=reduce,concat"
+# #export ASCEND_LAUNCH_BLOCKING=1
+# export SGLANG_ALLOW_OVERWRITE_LONGER_CONTEXT_LEN=1
+# export SGLANG_NPU_DISABLE_ACL_FORMAT_WEIGHT=1
+# unset HTTP_PROXY HTTPS_PROXY http_proxy https_proxy
+# export AUTOFUSE_FLAGS="--enable_autofuse=true;--autofuse_enable_pass=reduce,concat"
 
 export HCCL_BUFFSIZE=1024
-export SGLANG_DEEPEP_NUM_MAX_DISPATCH_TOKENS_PER_RANK=256
-export SGLANG_DEEPEP_BF16_DISPATCH=1
-export SGLANG_DEBUG_GRAPH_CAN_RUN=1
+# export SGLANG_DEEPEP_NUM_MAX_DISPATCH_TOKENS_PER_RANK=256
+# export SGLANG_DEEPEP_BF16_DISPATCH=1
+# export SGLANG_DEBUG_GRAPH_CAN_RUN=1
 
 python -m sglang.launch_server \
-        --model-path /home/ma-user/work/z84301856/models/LLaDA2.1-mini \
+        --model-path /data/public_models/LLaDA/LLaDA2.1-mini \
 	--served-model-name LLaDA2.1-mini \
         --host 0.0.0.0 \
         --port 8001 \
@@ -25,7 +25,6 @@ python -m sglang.launch_server \
 	--trust-remote-code \
 	--mem-fraction-static 0.90 \
         --max-running-requests 4 \
-        --cuda-graph-max-bs 4 \
         --enable-tokenizer-batch-encode \
 	--skip-server-warmup \
         --enable-cache-report \
@@ -33,14 +32,15 @@ python -m sglang.launch_server \
         --ep 4 \
         --dp-size 1 \
         --moe-dp-size 1 \
-        --enforce-piecewise-cuda-graph \
-        --piecewise-cuda-graph-tokens 32 64 96 128 \
         --moe-a2a-backend deepep \
         --deepep-mode auto \
         --disable-radix-cache \
         --dllm-algorithm "JointThreshold" \
-	--dllm-algorithm-config /home/ma-user/work/z84301856/sglang-dllm/test/registered/dllm/joint_threshold.yaml 
-        
+        --dllm-algorithm-config /data/home/z84301856/proj_sglang/sglang_dllm_yuan/sglang-dllm/test/registered/dllm/joint_threshold.yaml
+        # --cuda-graph-max-bs 4 \
+        # --enforce-piecewise-cuda-graph \
+        # --piecewise-cuda-graph-tokens 32 64 96 128 \
+
         # --disable-cuda-graph
         # --dllm-algorithm "LowConfidence" \
         # --dllm-algorithm-config "/home/ma-user/work/z84301856/sglang-dllm/bashs/dllm_config_zxx.yaml" \        】
